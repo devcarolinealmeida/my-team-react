0e98d950f1de0044efd5490448c274d0b6c39178
devcarolinealmeida <carolineitalia85@gmail.com>
2022-08-24 12:29:11 +0200

    add banner and clean files

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Banner.css'
+
+const Banner = () => {
+    return ( 
+        <header className='banner'>
+            <img src="/images/banner.png" alt="Banner"/>
+        </header>
+     );
+}
+ 
+export default Banner;
