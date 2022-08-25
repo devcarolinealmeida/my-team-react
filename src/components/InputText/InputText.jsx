6e78c5b80dd889df1a2a11549f6ffc0e564f17e6
devcarolinealmeida <carolineitalia85@gmail.com>
2022-08-25 12:53:05 +0200

    add handleText

--- src/components/InputText/InputText.jsx
+++ src/components/InputText/InputText.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './InputText.css'
 
 const InputText = (props) => {
+
+    const [text, setText] = useState('')
+
+    const handleText = (event) => {
+        setText(event.target.value)
+        console.log(text)
+    }
     return ( 
         <div className='input-text'>
             <label>{props.label}</label>
-            <input required={props.requiredField} placeholder={props.placeholder}/>
+            <input value={props.text} onChange={handleText} required={props.requiredField} placeholder={props.placeholder}/>
         </div>
      );
 }
