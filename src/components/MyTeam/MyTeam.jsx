f065ec991ea470678dd9343c5ca1165cd7494ca3
devcarolinealmeida <carolineitalia85@gmail.com>
2022-08-30 13:46:46 +0200

    add props.players.length > 0 returns " "

--- src/components/MyTeam/MyTeam.jsx
+++ src/components/MyTeam/MyTeam.jsx
@@ -5,13 +5,13 @@ import './MyTeam.css'
 
 const MyTeam = (props) => {
     return ( 
-        <section className='container_my-team-position' style={{ backgroundColor: props.secondColor }}>
+        (props.players.length > 0) ? <section className='container_my-team-position' style={{ backgroundColor: props.secondColor }}>
             <h3 style={{borderBottomColor: props.firstColor}} >{props.positionName}</h3>
             <div className='my-team-position-card'>
                 {props.players.map( player => <Player name={player.name} team={player.team} image={player.image} />)}
             </div>
-           
         </section>
+        : ''
      );
 }
  
