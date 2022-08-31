302024d6b2a495d31f072346b07508a0181a1b87
devcarolinealmeida <carolineitalia85@gmail.com>
2022-08-31 12:43:08 +0200

    ajusting banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,6 +5,7 @@ import './Banner.css'
 const Banner = () => {
     return ( 
         <header className='banner'>
+            <img src="/images/banner_soccer.png" alt="" />
             <h2>Build your own Team</h2>
         </header>
      );
